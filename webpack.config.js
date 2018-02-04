933bdf25194057b6dd530ffe77d220cd0027900e
James Milner <jamesmilner22@hotmail.com>
2018-02-04 16:12:20 +0000

    Try this to get Safari working

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,27 @@
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const path = require("path");
 
 module.exports = {
-    mode : "production",
-
+    entry: "./src/entry.js",
+    output: {
+        // options related to how webpack emits results
+    
+        path: path.resolve(__dirname, "dist"), // string
+        // the target directory for all output files
+        // must be an absolute path (use the Node.js path module)
+    
+        filename: "main.js", // string
+    },
     plugins: [
       new UglifyJsPlugin({
         sourceMap: true,
         uglifyOptions: {
+            ecma: 6,
             mangle: {
                 safari10: true,
             },
             output: {
-                safari10: false,
+                safari10: true,
             },
         } 
       })
