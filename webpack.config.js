dbf8b37ce7b8c81ddc9c9eec2b9ab43f74de7e4f
James Milner <jamesmilner22@hotmail.com>
2018-02-04 00:55:58 +0000

    Try again

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,17 @@ const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
     mode : "production",
+
     plugins: [
       new UglifyJsPlugin({
+        sourceMap: true,
         uglifyOptions: {
             mangle: {
                 safari10: true,
-              }
+            },
+            output: {
+                safari10: false,
+            },
         } 
       })
     ]
